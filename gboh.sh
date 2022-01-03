b22f7cdb2cae60ea0dc99463d51f617d1c7980c8
LeMegiste <dev@megiste.ch>
2022-01-03 17:16:44 +0100

    Battle of Leuctra

--- gboh.sh
+++ gboh.sh
@@ -3,7 +3,7 @@ export JAVA_HOME=/Users/laurentkloetzer/code/jdk-11.0.12.jdk/Contents/Home/
 echo $JAVA_HOME
 
 
-export CMD="${JAVA_HOME}bin/java -jar target/gboh-companion.jar";
+export CMD="${JAVA_HOME}bin/java -jar target/gboh-companion.jar > logs.txt";
 
 echo $CMD;
 
